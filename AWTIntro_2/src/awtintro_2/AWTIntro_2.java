62a0b06dee3bfbf80224422faef5f8ab609c72f0
jeshc <hcjesus@gmail.com>
2020-05-06 16:40:21 -0500

    awt intro

---
+++ AWTIntro_2/src/awtintro_2/AWTIntro_2.java
@@ -0,0 +1,19 @@
+package awtintro_2;
+
+import ico.fes.poo.gui.Ventana;
+/**
+ *
+ * @author Monse
+ */
+public class AWTIntro_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana ven1 = new Ventana(400, 300, "Hola mundo AWT");
+        ven1.setVisible(true);
+    }
+    
+}
---
+++ AWTIntro_2/src/ico/fes/poo/gui/Ventana.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ico.fes.poo.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.HeadlessException;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+/**
+ *
+ * @author jeshc
+ */
+public class Ventana extends Frame {
+
+    private String titulo;
+    private int alto;
+    private int ancho;
+    private Button boton1;
+    private Button boton2;
+
+    public Ventana() throws HeadlessException {
+    }
+
+    public Ventana(int alto, int ancho, String title) throws HeadlessException {
+        super(title);
+        this.alto = alto;
+        this.ancho = ancho;
+        this.setSize(this.ancho, this.alto);
+        boton1 = new Button("Presioname 1");
+        boton2 = new Button("Presioname 2");
+        this.setLayout(new BorderLayout());
+        this.add("North", boton1);
+        this.add("South", boton2);
+        this.add("West", new Button("P-3"));
+        this.add("East", new Button("Presioname 4"));
+
+        boton1.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                   // super.mouseClicked(e); //To change body of generated methods, choose Tools | Templates.
+                   System.out.println("Evento clic raton");
+                   System.out.println(e.getX());
+                   System.out.println(e.getY());
+                  
+                   
+            }
+            
+        });
+
+    }
+
+}
