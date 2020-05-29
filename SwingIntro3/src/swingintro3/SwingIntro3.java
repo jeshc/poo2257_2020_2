c43e4fc22f4b5a6cd85875b8e7697e1fccfe47a0
jeshc <hcjesus@gmail.com>
2020-05-29 16:05:35 -0500

    swing intro 3

---
+++ SwingIntro3/src/swingintro3/SwingIntro3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swingintro3;
+
+import ico.fes.poo.gui.VentanaSwing;
+
+/**
+ *
+ * @author jeshc
+ */
+public class SwingIntro3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaSwing ven=new VentanaSwing();
+        ven.setVisible(true);
+    }
+    
+}
